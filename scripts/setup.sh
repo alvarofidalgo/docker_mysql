70699d9a048b3e36b70b75dd457208c0b66b6a97
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2018-01-29 23:32:31 +0100

    include mysqldump and file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -22,9 +22,9 @@ passwords=$(grep 'temporary password' /var/log/mysqld.log)
 IFS=' ' read -ra PASS_LINE <<< $passwords
 pass=${#PASS_LINE[@]}
 root_pass=${PASS_LINE[$pass-1]}
-echo "ers use password es  $DB_USER_PASS"
+
 expect scripts/create_database.expect $root_pass $NEW_MYSQL_PASS $DATA_BASE_NAME $DB_USER_NAME $DB_USER_PASS
 
-
+mysqldump  -h localhost -u $DB_USER_NAME  --password=$DB_USER_PASS $DATA_BASE_NAME > scripts/db.sql
 
 tail -f /var/log/mysqld.log
