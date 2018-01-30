372c2b57c7fbde074dbed5b6596983a6ed137f8f
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2018-01-30 21:28:41 +0100

    include host permissions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ set -e
 : ${DATA_BASE_NAME:="defaultDatabaseName"}
 : ${DB_USER_NAME:="defaultDbUserName"} 
 : ${DB_USER_PASS:="defaultUserPass"}
-
+: ${USER_HOST_ACCEPTS:="%"}
 if  !(mysqladmin ping) ; then
    sh /scripts/initalize_db.sh
 fi
@@ -23,7 +23,7 @@ IFS=' ' read -ra PASS_LINE <<< $passwords
 pass=${#PASS_LINE[@]}
 root_pass=${PASS_LINE[$pass-1]}
 
-expect scripts/create_database.expect $root_pass $NEW_MYSQL_PASS $DATA_BASE_NAME $DB_USER_NAME $DB_USER_PASS
+expect scripts/create_database.expect $root_pass $NEW_MYSQL_PASS $DATA_BASE_NAME $DB_USER_NAME $DB_USER_PASS $USER_HOST_ACCEPTS:
 
 mysql  -h localhost -u $DB_USER_NAME  --password=$DB_USER_PASS $DATA_BASE_NAME < scripts/db.sql
 
